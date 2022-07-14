c62181153429af7c90908776e1b479303b62dbfa
fortunatoRoncholeta <fortunatoroncholeta@gmail.com>
2022-07-14 16:04:12 -0300

    subindo pro heroku

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template,redirect
+from flask import Flask, render_template,redirect,request,flash
+from flask_mail import Mail, Message
+
 
 
 app = Flask(__name__)
+app.secret_key = 'DKRoncholeta'
+
 
 @app.route('/')
 def index():
